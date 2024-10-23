04d85134ed1de171b4474632d0f2761939726dab
remi godet <remgodet@outlook.fr>
2024-10-23 13:15:34 +0200

    dataset cifar rollback and reqs

--- helpers/datasets.py
+++ helpers/datasets.py
@@ -84,9 +84,9 @@ def load_data(dataset):
     if dataset == "svhn":
         X_train, y_train = train_dataset.data, train_dataset.labels
         X_test, y_test = test_dataset.data, test_dataset.labels
-    elif "cifar10" in dataset:  
-        X_train, y_train = train_dataset.train_data, train_dataset.train_labels
-        X_test, y_test = test_dataset.test_data, test_dataset.test_labels
+    # elif "cifar10" in dataset:  
+    #     X_train, y_train = train_dataset.train_data, train_dataset.train_labels
+    #     X_test, y_test = test_dataset.test_data, test_dataset.test_labels
     else: #maybe to be modified as above ?
         X_train, y_train = train_dataset.data, train_dataset.targets
         X_test, y_test = test_dataset.data, test_dataset.targets
